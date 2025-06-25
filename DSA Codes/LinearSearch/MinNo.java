844ea63efa50a9bf389ed88c69890b4ff8181cf2
Arpit <arpitdeosthale12@gmail.com>
2025-06-25 16:00:51 +0530

    push

--- DSA Codes/LinearSearch/MinNo.java
+++ DSA Codes/LinearSearch/MinNo.java
@@ -1,3 +1,5 @@
+package LinearSearch;
+
 public class MinNo {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,7,8,9,0};
--- DSA Codes/LinearSearch/searchInRange.java
+++ DSA Codes/LinearSearch/searchInRange.java
@@ -1,10 +1,12 @@
-public class LinearSearch{
-    public static void main(String[] args) {
+package LinearSearch;
+
+public class searchInRange {
+     public static void main(String[] args) {
         System.out.println("Hello, World!");
 
         int[] arr = {1, 2, 3, 4, 5};
         int x = 1; // Element to search for
-        int result = LinearSearchEnhanced(arr, x);
+        int result = LinearSearch(arr, x , 1 , 2 );
         switch (result) {
             case-1:
                 System.out.println("Element not found in the array.");
@@ -16,11 +18,11 @@ public class LinearSearch{
 
     }
 
-    static int LinearSearch(int[] arr, int x ){
+    static int LinearSearch(int[] arr,int x , int start , int end ){
         if (arr.length == 0 ){
             return -1; // Array is empty
         }
-        for (int i =0 ; i <arr.length; i++) {
+        for (int i =start ; i <end; i++) {
             if (arr[i] == x) {
                 return i; // Element found
             }
@@ -42,4 +44,6 @@ public class LinearSearch{
         return -1; // Element not found
     }
 }
+
+    
 
