e3d2756cbf7b184824464f53f595090126b74a7a
Arpit <arpitdeosthale12@gmail.com>
2025-06-25 19:42:01 +0530

    Arpit 2025-6-25:19:42:1

---
+++ DSA Codes/LinearSearch/EvenDigits.java
@@ -0,0 +1,27 @@
+package LinearSearch;
+
+public class EvenDigits {
+    public static void main(String[] args) {
+
+    }
+    static int findNumbers(int[] nums) {
+        int count = 0;
+        for(int num : nums) {
+            if (even(num)) {
+                count++;
+            }
+        }
+        return count;
+    }
+    private static boolean even(int num) {
+        int count = 0;
+        while(num>0){
+            num = num/10;
+            count++;
+        }
+        if(count%2==0){
+            return true;
+        }
+        return false;
+    }
+}
